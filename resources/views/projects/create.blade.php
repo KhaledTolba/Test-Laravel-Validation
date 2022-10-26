15ac7dcb901316d4107d47b418387326b514f62d
Khaled Ahmed <36667919+KhaledTolba@users.noreply.github.com>
2022-10-26 03:35:40 +0200

    Edit files

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -9,10 +9,18 @@
     Title:
     <br />
     <input type="text" name="title" />
-    <br /><br />
+    <br />
+    @error('name')
+        <small class="alert alert-danger">The name field is required.</small>
+    @enderror
+    <br />
     Description:
     <br />
     <input type="text" name="description" />
-    <br /><br />
+    <br />
+    @error('description')
+        <small class="alert alert-danger">The description field is required.</small>
+    @enderror
+    <br />
     <button type="submit">Save</button>
 </form>
